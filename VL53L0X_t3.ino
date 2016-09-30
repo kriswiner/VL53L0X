df61f8fe67e73a91caa8ca1e078510a813b75106
Kris Winer <kriswiner@users.noreply.github.com>
2016-09-30 12:06:34 -0700

    Update VL53L0X_t3.ino

--- VL53L0X_t3.ino
+++ VL53L0X_t3.ino
@@ -44,6 +44,13 @@
  *  @brief List of all the defined registers
  *  @{
  */
+// Record the current time to check an upcoming timeout against
+uint16_t timeout_start_ms;
+uint16_t io_timeout;
+#define startTimeout() (timeout_start_ms = millis())
+
+// Check if timeout is enabled (set to nonzero value) and has expired
+#define checkTimeoutExpired() (io_timeout > 0 && ((uint16_t)millis() - timeout_start_ms) > io_timeout)
 
 // These are the registers defined in ST Microelecronics' API
 // Note the registers have 2-bytes addresses, but for some reason I can only get sensible data out of the VL53L0X when
@@ -152,6 +159,7 @@
 /* equivalent to a range sigma of 655.35mm */
 
 #define VL53L0X_REG_GLOBAL_CONFIG_VCSEL_WIDTH          0x032
+
 #define VL53L0X_REG_GLOBAL_CONFIG_SPAD_ENABLES_REF_0   0x0B0
 #define VL53L0X_REG_GLOBAL_CONFIG_SPAD_ENABLES_REF_1   0x0B1
 #define VL53L0X_REG_GLOBAL_CONFIG_SPAD_ENABLES_REF_2   0x0B2
@@ -163,7 +171,7 @@
 #define VL53L0X_REG_DYNAMIC_SPAD_NUM_REQUESTED_REF_SPAD 0x4E /* 0x14E */
 #define VL53L0X_REG_DYNAMIC_SPAD_REF_EN_START_OFFSET    0x4F /* 0x14F */
 #define VL53L0X_REG_POWER_MANAGEMENT_GO1_POWER_FORCE    0x80
-     #define VL53L0X_POWERMODE_STANDBY        0x00 //set power mode to standy level 1
+     #define VL53L0X_POWERMODE_STANDBY        0x00 //set power mode to standby level 1
      #define VL53L0X_POWERMODE_IDLE           0x01 //set power mode to idle level 1
 
 
@@ -188,6 +196,8 @@ int intPin = 9;
 bool newData = false;
 uint8_t rangeData[14];
 uint8_t rawData[4] = {0, 0, 0, 0};
+uint32_t lastUpdate = 0, Now = 0;          // used to calculate sample data rate
+
 
 #define verboseMode  true
 
@@ -206,7 +216,7 @@ void setup()
   pinMode(intPin, INPUT);
 
   byte HVI2C = readByte(VL53L0X_ADDRESS, VL53L0X_REG_VHV_CONFIG_PAD_SCL_SDA__EXTSUP_HV);
-  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_VHV_CONFIG_PAD_SCL_SDA__EXTSUP_HV, HVI2C & 0x01); // set I2C HIGH to 2.8 V
+  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_VHV_CONFIG_PAD_SCL_SDA__EXTSUP_HV, HVI2C | 0x01); // set I2C HIGH to 2.8 V
 
   I2Cscan();
   
@@ -229,16 +239,179 @@ void setup()
 
   writeByte(VL53L0X_ADDRESS, VL53L0X_REG_SOFT_RESET_GO2_SOFT_RESET_N, 0x01);  // reset device
   delay(100);
-  
-  // Configure GPIO1 for interrupt, active HIGH
-  byte actHIGH = readByte(VL53L0X_ADDRESS, VL53L0X_REG_GPIO_HV_MUX_ACTIVE_HIGH);
-  Serial.print("Active High byte = "); Serial.println(actHIGH);
-  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_GPIO_HV_MUX_ACTIVE_HIGH, actHIGH | 0x10); // GPIO1 interrupt active HIGH
-  actHIGH = readByte(VL53L0X_ADDRESS, VL53L0X_REG_GPIO_HV_MUX_ACTIVE_HIGH);
-  Serial.print("Active High byte = "); Serial.println(actHIGH);
-  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_SYSTEM_INTERRUPT_CONFIG_GPIO, 0x04); // enable data ready interrupt 
 
-  // Get some basic infor about the sensor
+  HVI2C = readByte(VL53L0X_ADDRESS, VL53L0X_REG_VHV_CONFIG_PAD_SCL_SDA__EXTSUP_HV);
+  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_VHV_CONFIG_PAD_SCL_SDA__EXTSUP_HV, HVI2C | 0x01); // set I2C HIGH to 2.8 V
+
+  if ((HVI2C & ~0xFE) == 0x00) Serial.println("I2C IO voltage is 1V8");
+  if ((HVI2C & ~0xFE) == 0x01) Serial.println("I2C IO voltage is 2V8");
+
+  // "Set I2C standard mode"
+  writeByte(VL53L0X_ADDRESS, 0x88, 0x00);
+
+  writeByte(VL53L0X_ADDRESS, 0x80, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0x00, 0x00);
+  uint8_t stop_variable = readByte(VL53L0X_ADDRESS, 0x91);
+  writeByte(VL53L0X_ADDRESS, 0x00, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x80, 0x00);
+
+
+// Taken from Pololu Arduino version of ST's API code
+
+  uint8_t spad_count;
+  bool spad_type_is_aperture;
+  getSpadInfo(&spad_count, &spad_type_is_aperture);
+//  if (!getSpadInfo(&spad_count, &spad_type_is_aperture)) { return false; }
+
+  // The SPAD map (RefGoodSpadMap) is read by VL53L0X_get_info_from_device() in
+  // the API, but the same data seems to be more easily readable from
+  // GLOBAL_CONFIG_SPAD_ENABLES_REF_0 through _6, so read it from there
+  uint8_t ref_spad_map[6];
+  readBytes(VL53L0X_ADDRESS, VL53L0X_REG_GLOBAL_CONFIG_SPAD_ENABLES_REF_0, 6, ref_spad_map);
+
+  // -- VL53L0X_set_reference_spads() begin (assume NVM values are valid)
+
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x01);
+  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_DYNAMIC_SPAD_REF_EN_START_OFFSET, 0x00);
+  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_DYNAMIC_SPAD_NUM_REQUESTED_REF_SPAD, 0x2C);
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x00);
+  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_GLOBAL_CONFIG_REF_EN_START_SELECT, 0xB4);
+
+  uint8_t first_spad_to_enable = spad_type_is_aperture ? 12 : 0; // 12 is the first aperture spad
+  uint8_t spads_enabled = 0;
+
+  for (uint8_t i = 0; i < 48; i++)
+  {
+    if (i < first_spad_to_enable || spads_enabled == spad_count)
+    {
+      // This bit is lower than the first one that should be enabled, or
+      // (reference_spad_count) bits have already been enabled, so zero this bit
+      ref_spad_map[i / 8] &= ~(1 << (i % 8));
+    }
+    else if ((ref_spad_map[i / 8] >> (i % 8)) & 0x1)
+    {
+      spads_enabled++;
+    }
+  }
+
+  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_GLOBAL_CONFIG_SPAD_ENABLES_REF_0, ref_spad_map[0]);
+  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_GLOBAL_CONFIG_SPAD_ENABLES_REF_1, ref_spad_map[1]);
+  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_GLOBAL_CONFIG_SPAD_ENABLES_REF_2, ref_spad_map[2]);
+  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_GLOBAL_CONFIG_SPAD_ENABLES_REF_3, ref_spad_map[3]);
+  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_GLOBAL_CONFIG_SPAD_ENABLES_REF_4, ref_spad_map[4]);
+  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_GLOBAL_CONFIG_SPAD_ENABLES_REF_5, ref_spad_map[5]);
+
+  // -- VL53L0X_set_reference_spads() end
+/*
+  // -- VL53L0X_load_tuning_settings() begin
+  // DefaultTuningSettings from vl53l0x_tuning.h
+
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0x00, 0x00);
+
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x09, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x10, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x11, 0x00);
+
+  writeByte(VL53L0X_ADDRESS, 0x24, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0x25, 0xFF);
+  writeByte(VL53L0X_ADDRESS, 0x75, 0x00);
+
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0x4E, 0x2C);
+  writeByte(VL53L0X_ADDRESS, 0x48, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x30, 0x20);
+
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x30, 0x09);
+  writeByte(VL53L0X_ADDRESS, 0x54, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x31, 0x04);
+  writeByte(VL53L0X_ADDRESS, 0x32, 0x03);
+  writeByte(VL53L0X_ADDRESS, 0x40, 0x83);
+  writeByte(VL53L0X_ADDRESS, 0x46, 0x25);
+  writeByte(VL53L0X_ADDRESS, 0x60, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x27, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x50, 0x06);
+  writeByte(VL53L0X_ADDRESS, 0x51, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x52, 0x96);
+  writeByte(VL53L0X_ADDRESS, 0x56, 0x08);
+  writeByte(VL53L0X_ADDRESS, 0x57, 0x30);
+  writeByte(VL53L0X_ADDRESS, 0x61, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x62, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x64, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x65, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x66, 0xA0);
+
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0x22, 0x32);
+  writeByte(VL53L0X_ADDRESS, 0x47, 0x14);
+  writeByte(VL53L0X_ADDRESS, 0x49, 0xFF);
+  writeByte(VL53L0X_ADDRESS, 0x4A, 0x00);
+
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x7A, 0x0A);
+  writeByte(VL53L0X_ADDRESS, 0x7B, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x78, 0x21);
+
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0x23, 0x34);
+  writeByte(VL53L0X_ADDRESS, 0x42, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x44, 0xFF);
+  writeByte(VL53L0X_ADDRESS, 0x45, 0x26);
+  writeByte(VL53L0X_ADDRESS, 0x46, 0x05);
+  writeByte(VL53L0X_ADDRESS, 0x40, 0x40);
+  writeByte(VL53L0X_ADDRESS, 0x0E, 0x06);
+  writeByte(VL53L0X_ADDRESS, 0x20, 0x1A);
+  writeByte(VL53L0X_ADDRESS, 0x43, 0x40);
+
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x34, 0x03);
+  writeByte(VL53L0X_ADDRESS, 0x35, 0x44);
+
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0x31, 0x04);
+  writeByte(VL53L0X_ADDRESS, 0x4B, 0x09);
+  writeByte(VL53L0X_ADDRESS, 0x4C, 0x05);
+  writeByte(VL53L0X_ADDRESS, 0x4D, 0x04);
+
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x44, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x45, 0x20);
+  writeByte(VL53L0X_ADDRESS, 0x47, 0x08);
+  writeByte(VL53L0X_ADDRESS, 0x48, 0x28);
+  writeByte(VL53L0X_ADDRESS, 0x67, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x70, 0x04);
+  writeByte(VL53L0X_ADDRESS, 0x71, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0x72, 0xFE);
+  writeByte(VL53L0X_ADDRESS, 0x76, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x77, 0x00);
+
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0x0D, 0x01);
+
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x80, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0x01, 0xF8);
+
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0x8E, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0x00, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x80, 0x00);
+
+// -- VL53L0X_load_tuning_settings() end
+ */
+  
+  // Configure GPIO1 for interrupt, active LOW
+  byte actHIGH = readByte(VL53L0X_ADDRESS, VL53L0X_REG_GPIO_HV_MUX_ACTIVE_HIGH);
+  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_SYSTEM_INTERRUPT_CONFIG_GPIO, 0x04); // enable data ready interrupt 
+  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_GPIO_HV_MUX_ACTIVE_HIGH, actHIGH & ~0x10); // GPIO1 interrupt active LOW
+  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_SYSTEM_INTERRUPT_CLEAR, 0x01); // clear interrupt   
+
+  // Get some basic information about the sensor
   byte val1 = readByte(VL53L0X_ADDRESS, VL53L0X_REG_PRE_RANGE_CONFIG_VCSEL_PERIOD);
   Serial.print("PRE_RANGE_CONFIG_VCSEL_PERIOD="); Serial.println(val1); 
   Serial.print(" decode: "); Serial.println(VL53L0X_decode_vcsel_period(val1));
@@ -251,7 +424,9 @@ void setup()
   uint32_t IMPeriod = (((uint32_t) rawData[0]) << 24 | ((uint32_t) rawData[1]) << 16 | ((uint32_t) rawData[2]) << 8 | rawData[3]);
   Serial.print(" System Intermeasurement period = "); Serial.print(IMPeriod); Serial.println(" ms");
   
-  attachInterrupt(intPin, myinthandler, RISING);  // define interrupt for GPI01 pin output of VL53L0X
+  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_SYSRANGE_START, 0x02); // continuous mode and arm next shot
+
+  attachInterrupt(intPin, myinthandler, FALLING);  // define interrupt for GPI01 pin output of VL53L0X
 
   }
   else
@@ -263,16 +438,18 @@ void setup()
 }
 
 void loop()
-{  
-  writeByte(VL53L0X_ADDRESS, VL53L0X_REG_SYSRANGE_START, 0x02); // continuous mode and arm next shot
-  
- // byte intStatus = readByte(VL53L0X_ADDRESS, VL53L0X_REG_RESULT_RANGE_STATUS);// Poll for data ready
- // if(intStatus & 0x01) // poll for data ready
-  
-  if (newData) // wait for data ready interrupt
-  newData = false;
+{    
+//  byte intStatus = readByte(VL53L0X_ADDRESS, VL53L0X_REG_RESULT_RANGE_STATUS);// Poll for data ready
+//  if(intStatus & 0x01) // poll for data ready
+//  {
+    
+ if(newData) // wait for data ready interrupt  
+ {
+ newData = false;
+ Now = micros();
+ Serial.print("data rate = "); Serial.print(1000000./(Now - lastUpdate)); Serial.println(" Hz");
+ writeByte(VL53L0X_ADDRESS, VL53L0X_REG_SYSTEM_INTERRUPT_CLEAR, 0x01); // clear interrupt   
 
-  {
  readBytes(VL53L0X_ADDRESS, VL53L0X_REG_RESULT_RANGE_STATUS, 14, &rangeData[0]); // continuous ranging
 
  Serial.print("byte 1 = "); Serial.println(rangeData[0]);
@@ -309,16 +486,15 @@ void loop()
   if(devError == 0x0D)  Serial.println("ALGO OVERFLOW!");
   if(devError == 0x0E)  Serial.println("RANGE IGNORE THRESHOLD!");
   
- Serial.print("Effective SPAD Return Count = "); Serial.println( ((float) (rangeData[2]) + (float)rangeData[3]/255.), 2); // 8.8 format
- Serial.print("Signal Rate = "); Serial.print( (uint16_t) ((uint16_t) rangeData[6] << 8) | rangeData[7]); Serial.println(" mega counts per second");
- Serial.print("Ambient Rate = "); Serial.print( (uint16_t) ((uint16_t) rangeData[8] << 8) | rangeData[9]); Serial.println(" mega counts per second");
- Serial.print("Distance = "); Serial.print( (uint16_t) ((uint16_t) rangeData[10] << 8) | rangeData[11]); Serial.println(" mm");
+  Serial.print("Effective SPAD Return Count = "); Serial.println( ((float) (rangeData[2]) + (float)rangeData[3]/255.), 2); // 8.8 format
+  Serial.print("Signal Rate = "); Serial.print( (uint16_t) ((uint16_t) rangeData[6] << 8) | rangeData[7]); Serial.println(" mega counts per second");
+  Serial.print("Ambient Rate = "); Serial.print( (uint16_t) ((uint16_t) rangeData[8] << 8) | rangeData[9]); Serial.println(" mega counts per second");
+  Serial.print("Distance = "); Serial.print( (uint16_t) ((uint16_t) rangeData[10] << 8) | rangeData[11]); Serial.println(" mm");
 
-
-    
-    digitalWrite(myLed, !digitalRead(myLed)); 
-    delay(500);
-}
+   lastUpdate = Now;
+   
+   digitalWrite(myLed, !digitalRead(myLed));
+    }
 }
 
 //===================================================================================================================
@@ -328,6 +504,49 @@ void loop()
 void myinthandler()
 {
   newData = true;
+}
+
+// Get reference SPAD (single photon avalanche diode) count and type
+// based on VL53L0X_get_info_from_device(),
+// but only gets reference SPAD count and type
+bool getSpadInfo(uint8_t * count, bool * type_is_aperture)
+{
+  uint8_t tmp;
+
+  writeByte(VL53L0X_ADDRESS, 0x80, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0x00, 0x00);
+
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x06);
+  writeByte(VL53L0X_ADDRESS, 0x83, readByte(VL53L0X_ADDRESS, 0x83) | 0x04);
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x07);
+  writeByte(VL53L0X_ADDRESS, 0x81, 0x01);
+
+  writeByte(VL53L0X_ADDRESS, 0x80, 0x01);
+
+  writeByte(VL53L0X_ADDRESS, 0x94, 0x6b);
+  writeByte(VL53L0X_ADDRESS, 0x83, 0x00);
+  startTimeout();
+  while (readByte(VL53L0X_ADDRESS, 0x83) == 0x00)
+  {
+    if (checkTimeoutExpired()) { return false; }
+  }
+  writeByte(VL53L0X_ADDRESS, 0x83, 0x01);
+  tmp = readByte(VL53L0X_ADDRESS, 0x92);
+
+  *count = tmp & 0x7f;
+  *type_is_aperture = (tmp >> 7) & 0x01;
+
+  writeByte(VL53L0X_ADDRESS, 0x81, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x06);
+  writeByte(VL53L0X_ADDRESS, 0x83, readByte(VL53L0X_ADDRESS,  0x83  & ~0x04));
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x01);
+  writeByte(VL53L0X_ADDRESS, 0x00, 0x01);
+
+  writeByte(VL53L0X_ADDRESS, 0xFF, 0x00);
+  writeByte(VL53L0X_ADDRESS, 0x80, 0x00);
+
+  return true;
 }
 
 uint16_t VL53L0X_decode_vcsel_period(short vcsel_period_reg) {
@@ -383,7 +602,7 @@ void I2Cscan()
 // I2C read/write functions for the VL53L0X sensor
 // VL53L0X has 16-bit register addresses, so the MSB of the register is sent first, then the LSB
 
-        void writeByte(uint8_t address, uint16_t subAddress, uint8_t data)
+void writeByte(uint8_t address, uint16_t subAddress, uint8_t data)
 {
 	Wire.beginTransmission(address);      // Initialize the Tx buffer
 //	Wire.write((subAddress >> 8) & 0xFF); // Put MSB of 16-bit slave register address in Tx buffer
@@ -392,7 +611,7 @@ void I2Cscan()
 	Wire.endTransmission();               // Send the Tx buffer
 }
 
-        uint8_t readByte(uint8_t address, uint16_t subAddress)
+uint8_t readByte(uint8_t address, uint16_t subAddress)
 {
 	uint8_t data; // `data` will store the register data	 
 	Wire.beginTransmission(address);         // Initialize the Tx buffer
@@ -406,7 +625,7 @@ void I2Cscan()
 	return data;                             // Return data read from slave register
 }
 
-        void readBytes(uint8_t address, uint16_t subAddress, uint8_t count, uint8_t * dest)
+void readBytes(uint8_t address, uint16_t subAddress, uint8_t count, uint8_t * dest)
 {  
 	Wire.beginTransmission(address);      // Initialize the Tx buffer
 //	Wire.write((subAddress >> 8) & 0xFF); // Put MSB of 16-bit slave register address in Tx buffer
